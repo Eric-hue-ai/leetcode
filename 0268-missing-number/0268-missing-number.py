230242bab959e9110d59d128a0bd64e60e7535d4
Eric-hue-ai <Ericjtom06@gmail.com>
2026-09-21 21:58:38 +0530

    Time: 0 ms (100%), Space: 13.3 MB (68.02%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        n = len(nums)
+
+        expected = n * (n + 1) // 2
+        actual = sum(nums)
+
+        return expected - actual
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
